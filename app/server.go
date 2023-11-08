1e68ab68f64e9f9d94527d58ed85809abcd7c1b7
vijay <vijaycan@icloud.com>
2023-11-08 17:11:05 -0500

    handle connection

--- app/server.go
+++ app/server.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	// Uncomment this block to pass the first stage
-	 "net"
-	 "os"
+	"net"
+	"os"
 )
+
+func handleClient(conn net.Conn) {
+	_, err := conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("error command pong: ", err.Error())
+		os.Exit(1)
+	}
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,14 +21,17 @@ func main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	 l, err := net.Listen("tcp", "0.0.0.0:6379")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 6379")
-	 	os.Exit(1)
-	 }
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	for {
+		conn, err = l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handle(conn)
+	}
 }
